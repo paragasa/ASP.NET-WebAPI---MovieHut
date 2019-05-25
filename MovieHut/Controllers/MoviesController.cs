3b37ff5695efd607cc8b13dcbdf85aa98b4fdbc1
Alan Paul B. Paragas <alanpaulparagas@gmail.com>
2019-05-25 15:42:40 -0700

    Update to movies details and controller

--- MovieHut/Controllers/MoviesController.cs
+++ MovieHut/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,23 +11,52 @@ namespace MovieHut.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Lord of the Rings"};
 
-        var customers = new List<Customer>
-        {
-            new Customer {Name = "Customer 1"},
-            new Customer {Name = "Customer 2"}
-        };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-        var viewModel = new RandomMovieViewModel
+            var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
 
             return View(viewModel);
-    }
+        }
+        
+        // GET: Customers
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m=> m.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
     }
 }
