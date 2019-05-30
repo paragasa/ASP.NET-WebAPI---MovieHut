329764fd778b59d42f7fcde7ffda577cc9c08c7e
Alan Paul B. Paragas <alanpaulparagas@gmail.com>
2019-05-29 17:17:48 -0700

    added customer dto and automapping

---
+++ MovieHut/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using MovieHut.Dtos;
+using MovieHut.Models;
+
+namespace MovieHut.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+
+            //TODO create mapping for Movies
+        }
+    }
+}
--- MovieHut/Global.asax.cs
+++ MovieHut/Global.asax.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using MovieHut.App_Start;
 
 namespace MovieHut
 {
@@ -12,10 +15,12 @@ namespace MovieHut
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
         }
     }
 }
